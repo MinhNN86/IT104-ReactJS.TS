8ce122f7ff7fb77c86a6c5986b98afca2ae4b5e4
MinhNN86 <minhnguyennhatt@gmail.com>
2025-08-28 07:43:49 +0700

    Session19

---
+++ PTIT_CNTT1_IT104_Session19/src/components/PTIT_CNTT1_IT104_Session19_Bai01.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+interface CartItem {
+  id: number;
+  name: string;
+  price: number;
+  quantity: number;
+}
+
+export default function PTIT_CNTT1_IT104_Session19_Bai01() {
+  return <div>PTIT_CNTT1_IT104_Session19_Bai01</div>;
+}
