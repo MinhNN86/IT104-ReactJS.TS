a442cd9e246494fb60119388f95fcc8a48df5882
MinhNN <minhnguyennhatt@gmail.com>
2025-09-03 10:54:31 +0700

    Session22

---
+++ PTIT_CNTT1_IT104_Session22/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import PTIT_CNTT1_IT104_Session22_Bai01 from "./components/PTIT_CNTT1_IT104_Session22_Bai01";
+
+function App() {
+  return (
+    <>
+      <PTIT_CNTT1_IT104_Session22_Bai01 />
+    </>
+  );
+}
+
+export default App;
---
+++ PTIT_CNTT1_IT104_Session22/src/components/PTIT_CNTT1_IT104_Session22_Bai01.tsx
@@ -0,0 +1,15 @@
+import Button from "react-bootstrap/Button";
+
+export default function PTIT_CNTT1_IT104_Session22_Bai01() {
+  return (
+    <>
+      <Button variant="primary">Lưu</Button>
+      <Button variant="secondary">Huỷ</Button>
+      <Button variant="success">Thành công</Button>
+      <Button variant="warning">Cảnh báo</Button>
+      <Button variant="danger">Báo lỗi</Button>
+      <Button variant="info">Thông tin</Button>
+      <Button variant="link">Đường dẫn</Button>
+    </>
+  );
+}
